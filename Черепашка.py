9b6fb4c13c9fe34e635e0776c92aec16487f769d
AlexandraPetuhova <petukhova_alexandra@mail.ru>
2023-10-14 00:01:56 +0300

    игра в черепашку

--- Черепашка.py
+++ Черепашка.py
@@ -1,26 +1,60 @@
 import turtle
-
 x = int(input('Введите число '))
 from turtle import *
 q = ('red', 'magenta', 'MediumOrchid', 'BlueViolet', 'blue', 'DodgerBlue', 'DarkTurquoise', 'lime green', 'GreenYellow', 'yellow', 'Gold', 'DarkOrange')
 shape('turtle')
 pensize(2)
-f = 10
-while True:
-    for i in q:
-        pencolor(i)
-        if x % 2 == 1:
-            left(30)
-            forward(f)
-            x = x * 3 + 1
-        else:
-            right(30)
-            forward(f)
-            x = x / 2
-# 657187189415641
-
-from turtle import *
-pensize(2)
-speed(15)
-# ****
-exitonclick()
+score = 0
+fxm, fym = 900, 900
+try:
+    while True:
+        for i in q:
+            if x == 1:
+                begin_fill()
+                for i in range(3):
+                    for i in q:
+                        pencolor(i)
+                        if x % 2 == 1:
+                            left(30)
+                            forward(10)
+                            x = x * 3 + 1
+                            if fxm > xcor():
+                                fxm = xcor()
+                            if fym > ycor():
+                                fym = ycor()
+                        else:
+                            right(30)
+                            forward(10)
+                            x = x / 2
+                            if fxm > xcor():
+                                fxm = xcor()
+                            if fym > ycor():
+                                fym = ycor()
+                end_fill()
+                hideturtle()
+                speed(0)
+                penup()
+                setpos(fxm, fym)
+                setheading(78)
+                forward(46)
+                FONT = ("Arial", 14)
+                pencolor('red')
+                pendown()
+                write("Game over", font=FONT)
+                exitonclick()
+                print("Game over")
+                print(f"Your score is {score}")
+            else:
+                pencolor(i)
+                if x % 2 == 1:
+                    left(30)
+                    forward(10)
+                    x = x * 3 + 1
+                    score += 1
+                else:
+                    right(30)
+                    forward(10)
+                    x = x / 2
+                    score += 1
+except:
+    pass
